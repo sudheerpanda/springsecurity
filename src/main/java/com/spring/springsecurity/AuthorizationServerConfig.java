47303fdfb054b226e6defafa1ff57c6081e5a832
sudheerpanda <sudheer.panda@imaginea.com>
2018-02-20 17:43:25 +0530

    added oauth capable and server config

---
+++ src/main/java/com/spring/springsecurity/AuthorizationServerConfig.java
@@ -0,0 +1,24 @@
+package com.spring.springsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("authanticationManger")
+    private AuthenticationManager authenticationManager;
+
+
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    }
+
+}
